67c671d23375080b6bc4df9a137dffba7d8bc487
Max Cyprys <mc.cyprysik@icloud.com>
2024-12-07 00:51:03 +0100

    feat: adding new variables characterizing the environment to RocketConfig

--- config.py
+++ config.py
@@ -7,3 +7,9 @@ class RocketConfig:
     fuel = 20  # kgs
     max_thrust = 1000  # kN
     burn_rate = 5  # kgs/s
+
+    starting_velocity_range = (10, 20)  # values are automatically converted to negative
+    starting_angle_range = (-30, 30)
+    starting_position_x = (0, 0)
+    starting_position_y = (80, 100)
+    disable_random_coords: bool = False
